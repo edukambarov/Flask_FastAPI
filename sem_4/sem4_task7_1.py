062cc7a023c6fe63c1436529714de2ba94aecfc3
edukambarov <edukambarovgb@yandex.ru>
2024-05-30 16:06:38 +0300

    sem4_residual

---
+++ sem_4/sem4_task7_1.py
@@ -0,0 +1,40 @@
+# Задание №7
+
+# Напишите программу на Python, которая будет находить
+# сумму элементов массива из 1000000 целых чисел.
+# � Пример массива: arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...]
+# � Массив должен быть заполнен случайными целыми числами
+# от 1 до 100.
+# � При решении задачи нужно использовать многопоточность,
+# многопроцессорность и асинхронность.
+# � В каждом решении нужно вывести время выполнения
+# вычислений.
+
+from random import randint
+import threading, multiprocessing, asyncio
+import time
+
+main_lst = [randint(1,101) for _ in range(1_000_000)]
+counter = 0
+
+def increment(num_lst):
+    global counter
+    for num in num_lst:
+        counter += num
+    print(counter)
+
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    threads = []
+    lsts_num = [main_lst[i:i+100_000] for i in range(10)]
+    for i in range(10):
+        t = threading.Thread(target=increment, args=(lsts_num[i],))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    print(f'All threads completed work. Time spent: {time.time() -start_time:0.02f} seconds.')
